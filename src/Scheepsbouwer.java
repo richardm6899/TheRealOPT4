92df78af790602bf9ee75b713ba5913d98703c92
OT62 <21064334@student.hhs.nl>
2023-04-06 10:33:49 +0200

    Class Scheepsbouwer frame gemaakt.

--- src/Scheepsbouwer.java
+++ src/Scheepsbouwer.java
@@ -1,2 +1,46 @@
-public class Scheepsbouwer {
+import java.util.ArrayList;
+
+public class Scheepsbouwer extends Gebruiker{
+    //up to date
+    private ArrayList<KlantType>klantTypes;
+    private String Wachtwoord;
+
+    public Scheepsbouwer(String naam, String Wachtwoord, ArrayList<EssentieleOptie> essentieleOpties, ArrayList<ExtraOptie> extraOptie, ArrayList<KlantType> klantTypes) {
+        super(naam, essentieleOpties, extraOptie);
+        this.klantTypes = klantTypes;
+        this.Wachtwoord = Wachtwoord;
+    }
+
+
+    public void printKlantType(){
+
+    }
+    //-----------------------------------------------------------------------------------
+    public void addEssentieleOptie(ArrayList<EssentieleOptie>essentieleOpties){
+
+    }
+
+    public void deleteEssentieleOptie(ArrayList<EssentieleOptie>essentieleOpties){
+
+    }
+    //-------------------------------------------------------------------------------------
+
+    public void addExtraOpties(ArrayList<ExtraOptie>extraOpties){
+
+    }
+
+    public void deleteExtraOpties(ArrayList<ExtraOptie>extraOpties){
+
+    }
+    //--------------------------------------------------------------------------------------
+    public void addKlantType(ArrayList<KlantType>klantTypes){
+
+    }
+
+    public void deleteKlantType(ArrayList<KlantType>klantTypes){
+
+    }
+
+
+
 }
