73bf17236c35ad0ef83b1b9497b7e2823510b611
Icedev1 <benaissa.sayfeddine@gmail.com>
2023-04-06 10:23:58 +0200

    h

--- src/MilieuKorting.java
+++ src/MilieuKorting.java
@@ -1,2 +1,17 @@
 public class MilieuKorting {
+    protected int kortingPercentage;
+    protected String beschrijving;
+
+    public MilieuKorting(int kortingPercentage) {
+        this.kortingPercentage = kortingPercentage;
+    }
+    public MilieuKorting(int kortingPercentage, String beschrijving) {
+        this.kortingPercentage = kortingPercentage;
+        this.beschrijving = beschrijving;
+    }
+
+    public int getKortingsPercentage() {return kortingPercentage;}
+    public void setKortingsPercentage(int kortingsPercentage) {this.kortingPercentage = kortingsPercentage;}
+    public String getBeschrijving() {return beschrijving;}
+    public void setBeschrijving(String beschrijving) {this.beschrijving = beschrijving;}
 }
