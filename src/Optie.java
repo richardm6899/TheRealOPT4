6e2f55d37b567ff52100216af456844b1afee989
richardm6899 <richardmorrissw@gmail.com>
2023-04-06 09:59:57 +0200

    Optie pushed

---
+++ src/Optie.java
@@ -0,0 +1,16 @@
+ class Optie {
+        private String omschrijvingoptie;
+
+        public Optie(String omschrijvingoptie) {
+            this.omschrijvingoptie = omschrijvingoptie;
+        }
+
+        public String getOmschrijvingOptie() {
+            return omschrijvingoptie;
+        }
+
+        public void setOmschrijvingOptie(String omschrijvingoptie) {
+            this.omschrijvingoptie = omschrijvingoptie;
+        }
+    }
+
