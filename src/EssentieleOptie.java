3e173eedac5c1a904143f333df05b1e5fbec8579
richardm6899 <richardmorrissw@gmail.com>
2023-04-06 10:18:11 +0200

    EssentieleOptie en ExtraOptie

--- src/EssentieleOptie.java
+++ src/EssentieleOptie.java
@@ -1,2 +1,16 @@
-public class EssentieleOptie {
+public class EssentieleOptie extends Optie{
+    private String essentieleoptie;
+
+    public EssentieleOptie(String essentieleoptie, String omschrijvingoptie) {
+        super(omschrijvingoptie);
+        this.essentieleoptie = essentieleoptie;
+    }
+
+    public String getEssentieleOptie() {
+        return essentieleoptie;
+    }
+
+    public void setEssentieleOptie(String essentieleoptie) {
+        this.essentieleoptie = essentieleoptie;
+    }
 }
