87b6b69d6b2bf7ed04b8088a600275a2f333d40d
Icedev1 <benaissa.sayfeddine@gmail.com>
2023-04-06 10:27:08 +0200

    h

--- src/KlantType.java
+++ src/KlantType.java
@@ -1,2 +1,17 @@
-public class KlantType {
+public class KlantType extends MilieuKorting{
+    private String klanttype;
+
+
+    public KlantType(String klanttype, int kortingpercentage) {
+        super(kortingpercentage);
+        this.klanttype= klanttype;
+    }
+
+    public String getKlanttype() {
+        return klanttype;
+    }
+
+    public void setKlanttype(String klanttype) {
+        this.klanttype = klanttype;
+    }
 }
